573b84bc9f9ebde1dcbee8fa23ad1a9687d683f1
Diego R Vila <dego_vila@yahoo.com>
2024-12-09 22:14:53 -0400

    refactor: starship

--- home.nix
+++ home.nix
@@ -6,84 +6,44 @@
   home.username = "ruahman";
   home.homeDirectory = "/home/ruahman";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.htop
     pkgs.neofetch
     pkgs.xclip
     pkgs.unzip
     pkgs.ripgrep
+    pkgs.fzf
     pkgs.ispell
     pkgs.lua
     pkgs.luarocks 
     pkgs.nodejs_22
     pkgs.zsh
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FiraCode" "CascadiaCove" "JetBrainsMono" ]; })
     pkgs.nerd-fonts.fira-code
     pkgs.nerd-fonts.caskaydia-cove
     pkgs.nerd-fonts.jetbrains-mono
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  # home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home.file = {
+    # file needs to be added to git before you can refrence it.
+    ".config/starship.toml".source = ./starship.toml;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  # };
+  };
 
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ruahman/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
   programs.emacs.enable = true;
   programs.neovim.enable = true;
   programs.lazygit.enable = true;
